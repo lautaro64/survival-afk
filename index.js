b1ed581dc302a4c71b77ab37f3712f8da566e247
lautaro64 <114043934+lautaro64@users.noreply.github.com>
2026-08-12 19:18:06 -0300

    index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+const mineflayer = require('mineflayer')
+
+const HOST = 'theo.hidencloud.com'
+const PORT = 24587
+
+const USERNAME = 'LautiMC'
+
+function createBot() {
+    console.log('[BOT] Conectando...')
+
+    const bot = mineflayer.createBot({
+        host: HOST,
+        port: PORT,
+        username: USERNAME,
+        version: '1.20.1'
+    })
+
+    bot.once('spawn', () => {
+        console.log('[BOT] Conectado al proxy.')
+
+        setTimeout(() => {
+            console.log('[BOT] Entrando a Survival...')
+            bot.chat('/server survival')
+        }, 3000)
+    })
+
+    bot.on('messagestr', (message) => {
+        console.log('[CHAT]', message)
+    })
+
+    bot.on('kicked', (reason) => {
+        console.log('[BOT] Expulsado:', reason)
+    })
+
+    bot.on('error', (err) => {
+        console.log('[BOT] Error:', err.message)
+    })
+
+    bot.on('end', () => {
+        console.log('[BOT] Desconectado. Reconectando en 10 segundos...')
+
+        setTimeout(() => {
+            createBot()
+        }, 10000)
+    })
+}
+
+createBot()
